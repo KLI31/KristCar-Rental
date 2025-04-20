e3c8e7e4018d8906aa66c8580b7c2ac622ee2fbe
Luis David <lrambao5432@gmail.com>
2025-04-20 12:48:15 -0500

    feat: Mejorar el hook useCars con opciones de revalidación y agregar función formatPrice para formatear precios en COP

--- hooks/use-cars.ts
+++ hooks/use-cars.ts
@@ -5,7 +5,11 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useCars = () => {
-  const { data, error, isLoading, mutate } = useSWR("/api/car", fetcher);
+  const { data, error, isLoading, mutate } = useSWR("/api/car", fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    dedupingInterval: 60000, // Revalidar cada minuto
+  });
 
   return {
     cars: data || [],
