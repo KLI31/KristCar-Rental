c795e1a2cc9add2b0e77bb52a40f8850d6f6d2ab
Luis David <lrambao5432@gmail.com>
2025-04-21 20:18:07 -0500

    feat: Agregar framer-motion como dependencia y ajustar la configuración de Tailwind para incluir rutas de componentes adicionales

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,9 @@ export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
